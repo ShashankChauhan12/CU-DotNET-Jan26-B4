e90b059e122ea9a7d705fd81792f785f7416bb03
Shashank Chauhan <shashankchauhan0160@gmail.com>
2026-01-12 14:20:34 +0530

    Added Day6-Greeting Solution with app and library

---
+++ Day6/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+
+            string message = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(message);
+        }
+    }
+}
